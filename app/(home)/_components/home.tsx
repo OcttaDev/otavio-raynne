860c2bcf51fd7f7dcbcca532f0732372e39313bd
Otavio <aotavio061@gmail.com>
2024-08-26 06:22:37 -0300

    test

--- app/(home)/_components/home.tsx
+++ app/(home)/_components/home.tsx
@@ -9,17 +9,26 @@ const forum = Forum({ weight: "400", subsets: ["latin"] });
 const great = Great_Vibes({ weight: "400", subsets: ["latin"] });
 
 export default function Home() {
+  const text = `"Alegrem-se na esperança, sejam pacientes na tribulação, perseverem
+            na oração. Rm 12:12"`;
   return (
     <main className="w-full">
       <header className="w-full h-[600px] flex flex-col items-center justify-center ">
         <Image
-          src="/elements/flor.svg"
+          src="/elements/flor-fundo.svg"
           alt="gifts"
-          width={200}
+          width={160}
           height={0}
-          className="absolute -top-16 right-0"
+          className="absolute -top-0 right-0 -rotate-[5deg]"
         />
-        <div className="flex flex-col items-center gap-10">
+        <Image
+          src="/elements/panelas.svg"
+          alt="gifts"
+          width={80}
+          height={0}
+          className="absolute -top-2 left-[10rem] rotate-40"
+        />
+        <div className="flex flex-col items-center gap-10 mt-2">
           <h1 className={` ${great.className} text-5xl font-bold`}>Chá de</h1>
           <Image
             src="/elements/telhado.svg"
@@ -28,20 +37,32 @@ export default function Home() {
             height={0}
             className="absolute"
           />
-          <p className={` ${forum.className} font-bold text-2xl mt-5`}>NOVA</p>
+          <p
+            className={` ${forum.className} font-bold text-2xl mt-5 text-[#3f5736]`}
+          >
+            NOVA
+          </p>
           <h1 className={` ${great.className} text-5xl font-bold`}>
             Otávio e Raynne
           </h1>
+          <p className="max-w-sm text-center text-sm ">{text}</p>
         </div>
-        <div className="w-full flex justify-center mt-28">
+        <div className="w-full flex justify-center mt-10">
           <Link
             href="/auth"
-            className="p-2 bg-yellow-900/50 w-60 rounded-full shadow-xl shadow-black/20 text-center text-white text-xl font-bold"
+            className="p-2 bg-[#3f5736] w-60 rounded-full shadow-xl shadow-black/20 text-center text-white text-xl font-bold"
           >
             Prosseguir
           </Link>
         </div>
       </header>
+      <Image
+        src="/elements/flor-fundo.svg"
+        alt="gifts"
+        width={160}
+        height={0}
+        className="absolute -bottom-20 left-0 rotate-[130deg]"
+      />
     </main>
   );
 }
